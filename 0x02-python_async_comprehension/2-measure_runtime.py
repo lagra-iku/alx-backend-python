951fddbe827492c050f2807ad3e6fd72c135b632
Grace O. Nweke <lagra2001@gmail.com>
2024-03-19 23:00:55 +0000

    solving for task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+write a measure_runtime coroutine that will execute async_comprehension
+four times in parallel using asyncio.gather
+"""
+
+import asyncio
+import random
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """
+    a measure_runtime coroutine function
+    """
+    start_time = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_time = time.time()
+    return end_time - start_time
