8f679d6669b3da8a9b453efe32601140e893810a
Grace O. Nweke <lagra2001@gmail.com>
2024-03-21 21:48:20 +0000

    correcting pycodestyle for task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -9,6 +9,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
-    a funct task_wait_random that takes an int max_delay and returns a asyncio.Task
+    a funct task_wait_random that takes an int max_delay
     """
     return asyncio.create_task(wait_random(max_delay))
