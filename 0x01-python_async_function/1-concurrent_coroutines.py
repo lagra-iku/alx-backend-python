8466e59b1fcf80a2327feb1f212ae69a11b6fc33
Grace O. Nweke <lagra2001@gmail.com>
2024-03-21 21:33:45 +0000

    solving for task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+execute multiple coroutines at the same time with async
+"""
+
+import asyncio
+import random
+from typing import List, Union
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Async routine that spawns wait_random n times with the specified max_delay.
+    """
+    delays = []
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays
