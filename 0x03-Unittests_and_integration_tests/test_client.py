e1071d1e165f7cdcfa07b5dcc703d1140a6982ff
Grace O. Nweke <lagra2001@gmail.com>
2024-04-12 21:16:43 +0000

    solving for task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -24,5 +24,28 @@ class TestGithubOrgClient(unittest.TestCase):
         github_org_client = GithubOrgClient(org_name)
         result = github_org_client.org
 
-        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+        mock_get_json.assert_called_once_with(
+                f"https://api.github.com/orgs/{org_name}"
+        )
         self.assertEqual(result, test_response)
+
+    def test_public_repos_url(self):
+        """Mocking a property"""
+        mock_payload = {
+            "login": "example_org",
+            "repos_url": "https://api.github.com/orgs/example_org/repos"
+        }
+
+        with patch(
+            'client.GithubOrgClient.org',
+            new_callable=PropertyMock
+        ) as mock_org:
+            mock_org.return_value = mock_payload
+
+            github_org_client = GithubOrgClient("example_org")
+            result = github_org_client._public_repos_url
+
+            self.assertEqual(
+                result,
+                "https://api.github.com/orgs/example_org/repos"
+            )
