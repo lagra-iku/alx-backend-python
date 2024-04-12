6da72f7e4049eafaf7e72e33ea5e011ff6420bf5
Grace O. Nweke <lagra2001@gmail.com>
2024-04-12 21:26:58 +0000

    solving for task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -49,3 +49,21 @@ class TestGithubOrgClient(unittest.TestCase):
                 result,
                 "https://api.github.com/orgs/example_org/repos"
             )
+
+    def test_public_repos(self, mock_json):
+        """More patching"""
+        json_payload = [{"name": "Google"}, {"name": "Twitter"}]
+        mock_json.return_value = json_payload
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public:
+
+            mock_public.return_value = "hello/world"
+            test_class = GithubOrgClient('test')
+            result = test_class.public_repos()
+
+            check = [i["name"] for i in json_payload]
+            self.assertEqual(result, check)
+
+            mock_public.assert_called_once()
+            mock_json.assert_called_once()
